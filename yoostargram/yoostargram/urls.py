5cb74269133e889f51b69cca109f43224cbd6ff2
RDH <RDH@RDHui-MacBook-Air.local>
2016-03-29 02:28:31 +0900

    init : profile

--- yoostargram/yoostargram/urls.py
+++ yoostargram/yoostargram/urls.py
@@ -22,8 +22,9 @@ from yoostargram.views import *
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^/$', home, name='home'),
+    url(r'^$', home, name='home'),
     url(r'^login/$', LoginView.as_view(), name='login'),
     url(r'^logout/$', LogoutView.as_view(), name='logout'),
     url(r'^signup/$', SignupView.as_view(), name='signup'),
+    url(r'^profile/$', ProfileView.as_view(), name='profile'),
 ]

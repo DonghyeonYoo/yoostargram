d99974995def38d172488a1aab19a182e2450acc
RDH <RDH@RDHui-MacBook-Air.local>
2016-03-29 04:42:40 +0900

    init : PostListView

--- yoostargram/yoostargram/urls.py
+++ yoostargram/yoostargram/urls.py
@@ -3,11 +3,13 @@ from django.contrib import admin
 
 from users.views import *
 from yoostargram.views import *
+from posts.views import *
 
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', home, name='home'),
+    url(r'^posts/$', PostListView.as_view(), name='posts'),
     url(r'^login/$', LoginView.as_view(), name='login'),
     url(r'^logout/$', LogoutView.as_view(), name='logout'),
     url(r'^signup/$', SignupView.as_view(), name='signup'),

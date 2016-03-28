cd5e2db720a43d9d794d42062fd28fb9c9c6caa2
RDH <RDH@RDHui-MacBook-Air.local>
2016-03-28 22:13:14 +0900

    init: home page

--- yoostargram/yoostargram/urls.py
+++ yoostargram/yoostargram/urls.py
@@ -17,9 +17,11 @@ from django.conf.urls import url
 from django.contrib import admin
 
 from users.views import *
+from yoostargram.views import *
 
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^/$', home, name='home'),
     url(r'^login/$', LoginView.as_view(), name='login'),
 ]

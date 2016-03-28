e0938421cfe58c8b2cdfbcbcb99e1a633837a10c
RDH <RDH@RDHui-MacBook-Air.local>
2016-03-28 22:10:39 +0900

    update : login.py , login.html login.py : if user: return login page else : home page login. html add password and description

--- yoostargram/users/views/login.py
+++ yoostargram/users/views/login.py
@@ -1,6 +1,7 @@
-from django.contrib.auth import login
+from django.contrib.auth import login, authenticate
 from django.views.generic import View
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.core.urlresolvers import reverse
 
 
 class LoginView(View):
@@ -15,4 +16,15 @@ class LoginView(View):
         )
 
     def post(self, request):
-        pass
+        my_username = request.POST.get('my_username')
+        my_password = request.POST.get('my_password')
+
+        user = authenticate(
+                username=my_username,
+                password=my_password,
+        )
+
+        if user:
+            login(request, user)
+            return redirect(reverse('home'))
+        return redirect(reverse('login'))

e92d5d95d6f394510546a25ca87947a2b50d225d
RDH <RDH@RDHui-MacBook-Air.local>
2016-03-29 00:23:41 +0900

    update: conneting  logout url on  urls.py

--- yoostargram/yoostargram/urls.py
+++ yoostargram/yoostargram/urls.py
@@ -24,4 +24,5 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^/$', home, name='home'),
     url(r'^login/$', LoginView.as_view(), name='login'),
+    url(r'^logout/$', LogoutView.as_view(), name='logout'),
 ]
